ee5b1b55f6984042257351e6d3142a1e13b955bb
shouhei <shohei.nagamura@gmail.com>
2023-01-31 19:55:32 +0900

    Initial Commit

---
+++ Swift_janken/Swift_jankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_jankenApp.swift
+//  Swift_janken
+//
+//  Created by shouhei nagamura on 2023/01/31.
+//
+
+import SwiftUI
+
+@main
+struct Swift_jankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
